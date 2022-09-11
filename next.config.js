9c6c07ef3634adb6c20e84fb5c81cc74a2ea9227
edwardlamar <edwardsnell1982@gmail.com>
2022-09-11 16:22:27 -0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images:{
+    domains:['pbs.twimg.com',
+    'lh3.googleusercontent.com'
+    ]
+  }
+}
+
+module.exports = nextConfig
